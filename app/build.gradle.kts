67efcb9be3f0f84003d372fa4fd8a5efd606e51f
Plaugig <plaugig@gmail.com>
2025-09-04 20:12:28 +0500

    binding feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,33 +5,21 @@ plugins {
 
 android {
     namespace = "com.plaugig.todo2"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.plaugig.todo2"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
+        versionName = "1.0"git
         targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
         jvmTarget = "11"
+    }
+    buildFeatures {
+        viewBinding = true
     }
 }
 
