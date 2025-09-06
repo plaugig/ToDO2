f8a278d44ae12054674ecf759070deb9e5bc8021
George <george.d.kim@gmail.com>
2025-09-06 17:11:45 -0400

    Refactor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,13 +41,10 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.recyclerview)
-    implementation(libs.androidx.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
